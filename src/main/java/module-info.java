2e89e735bd07f3ccfae2d23286961fc34ba3aa18
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-07-22 20:10:15 +0530

    Adding Data from the Database to Student table

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.ijse.cmjd.samsapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires static lombok;
 
 
     opens com.ijse.cmjd.samsapp to javafx.fxml;
     exports com.ijse.cmjd.samsapp;
     exports controllers;
     opens controllers to javafx.fxml;
+    opens dto.tm to javafx.base;
+    exports dto.tm;
 }
