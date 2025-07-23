4c24abf3c458d0e2a16b01cd8cf67cb04962e1c7
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-07-23 20:42:22 +0530

    StudentDao and StudentDaoImpl

---
+++ src/main/java/dao/impl/StudentDaoImpl.java
@@ -0,0 +1,33 @@
+package dao.impl;
+
+import entity.StudentEntity;
+import ustom.StudentDao;
+
+import java.util.ArrayList;
+
+public class StudentDaoImpl implements StudentDao {
+    @Override
+    public boolean save(StudentEntity studentEntity) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean update(StudentEntity studentEntity) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws Exception {
+        return false;
+    }
+
+    @Override
+    public StudentEntity search(String s) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ArrayList<StudentEntity> getALL() throws Exception {
+        return null;
+    }
+}
---
+++ src/main/java/ustom/StudentDao.java
@@ -0,0 +1,7 @@
+package ustom;
+
+import dao.CrudDao;
+import entity.StudentEntity;
+
+public interface StudentDao extends CrudDao<StudentEntity,String> {
+}
