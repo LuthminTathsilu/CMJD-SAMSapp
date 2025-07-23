52bc8762dad57976264ef58545716c9ad838acf2
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-07-23 20:13:09 +0530

    Super Dao and Dao Factory creation

---
+++ src/main/java/dao/DaoFactory.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.lang.classfile.instruction.SwitchCase;
+
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+    private DaoFactory(){
+
+    }
+    public static DaoFactory getInstance(){
+        if (daoFactory==null){
+            daoFactory = new DaoFactory();
+        }
+        return daoFactory;
+    }
+    public SuperDao getDao(DaoTypes type){
+        switch (type){
+            case STUDENT:
+             return null;
+            case COURSE:
+                return null;
+            case ATTENDENCE:
+                return null;
+            case CLASS:
+                return null;
+            case LECTURER:
+                return null;
+            case USER:
+                return null;
+            case CLASS_LECTURER:
+                return null;
+            case SUBJECT:
+                return null;
+            default:
+                return null;
+        }
+
+    }
+    public enum DaoTypes{
+        STUDENT, COURSE,ATTENDENCE,CLASS,SUBJECT,LECTURER,USER,CLASS_LECTURER
+
+    }
+}
