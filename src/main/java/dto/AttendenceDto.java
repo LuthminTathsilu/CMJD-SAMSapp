70b42ba4a661b577a818aae71a38c0e2298fa7c0
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-07-23 07:43:42 +0530

    Creating Dtos

--- src/main/java/dto/AttendenceDto.java
+++ src/main/java/dto/AttendenceDto.java
@@ -1,4 +1,18 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AttendenceDto {
+    private String id;
+    private String studentId;
+    private String classId;
+    private String status;
+    private String markedTime;
+
+
 }
